ac82737365d6a1a2ea788e3b84f7b6e068aaf0e2
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-01 15:21:18 +0530

    document header added

---
+++ student_details1.py
@@ -0,0 +1,15 @@
+'''
+Python program to get the student details
+Author: Maria
+Date: 01-10-2024
+Version: 1.0
+'''
+print("hello")
+name=input("enter your name")
+roll_number=int(input("enter your roll_numer:"))
+roll_number=roll_number+1
+cgpa=float(input("enter your cgpa:"))
+percentage=cgpa*10
+print("name of the student:",name)
+print("student roll number:",roll_number)
+print(percentage,"%")
