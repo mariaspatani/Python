903037fedc5322b2d8db157a745853dac8211fb6
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-05 16:05:01 +0530

    Add files via upload

---
+++ python5.py
@@ -0,0 +1,19 @@
+"""
+program:numbers
+version:1.1
+date:5-5-2024
+a Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results
+"""
+
+
+
+number1=10
+number2=5
+print(number1+number2)
+print(number1/number2)
+is_positive=number1>number2
+A=number2==number1
+print(A)
+print(is_positive)
+print(number1>number2 and number2<number1)
+print(number1>number2 or number2>number1)
