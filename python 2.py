945fcb42f4b36883769ac016d0b6e066d18fa210
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-05 16:03:17 +0530

    Add files via upload

---
+++ python 2.py
@@ -0,0 +1,11 @@
+"""
+program name:John
+version:1.1
+date:5-10-2024
+This Python program that asks the user to input their name and age.
+After receiving the input, display a message that includes the user's name and age
+"""
+
+name= input("enter your name")
+age=input("enter your age")
+print("Hello,",name+"!", "You are",age,"years old.")
