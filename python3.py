e2cdfe7eeb87e9300fb1eae4c59ebd7e63bc5f0d
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-05 16:03:52 +0530

    Add files via upload

---
+++ python3.py
@@ -0,0 +1,17 @@
+"""
+program:Hello World
+version:1.1
+date:5-5-2024
+a Python program that stores a string in a variable.
+Extract a specific part of the string (substring) and then concatenate it with another string.
+ Finally, display the new string.
+"""
+
+
+
+
+
+
+Str1="Hello, World!"
+Str2="Everyone!"
+print(Str1[7:12],"",Str2)
