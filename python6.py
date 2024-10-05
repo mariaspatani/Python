87a6cc5eb6f27495dff4f93a6b84c8f48232f0d0
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-05 16:05:30 +0530

    Add files via upload

---
+++ python6.py
@@ -0,0 +1,13 @@
+"""
+program:import math
+version:1.1
+date:5-5-2024
+Python program that uses functions from the math module to perform the following operations on a number provided by the user
+"""
+
+
+
+import math
+print(math.sqrt(5))
+print(math.factorial(5))
+print(math.pow(5,2))
