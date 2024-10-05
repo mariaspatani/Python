314fc4af04121f0a4779f98abc8bad200b9b7109
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-05 16:04:25 +0530

    Add files via upload

---
+++ python4.py
@@ -0,0 +1,11 @@
+"""
+program name:twinkle twinkle
+version:1.1
+date:5-10-2024
+a Python program to print the following string in a specific format
+"""
+
+
+Str= "Twinkle, twinkle, little star,\n\t How I wonder what you are!\n\t\t Up above the world so high,\n\t\t Like a diamond in the sky.\n Twinkle, twinkle, little star,\n\t How I wonder what you are"
+print(Str)
+
