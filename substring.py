21c9089b50dfe3f5e1d8fd4c805140713412af38
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-08 14:52:47 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,17 @@
+"""
+Program name : concatenating string
+version: 1.1
+date: 8-10-2024
+"""
+
+
+
+first_name ="John"
+last_name ="Doe"
+full_name = first_name+" "+last_name
+print(full_name)
+length=len(first_name)
+print(length)
+extracted_last_name= full_name[length+1:]
+print(extracted_last_name)
+
