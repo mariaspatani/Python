7caebb92f0f73700e97bc7e4e453637053365da8
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-08 14:52:12 +0530

    Add files via upload

---
+++ extractedfirstname.py
@@ -0,0 +1,8 @@
+first_name= "Maria"
+last_name= "S"
+full_name= first_name+ " "+ last_name
+print(full_name)
+length= len(first_name)
+print(length)
+extracted_first_name= full_name[:length]
+print(extracted_first_name)
