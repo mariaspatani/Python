e1c6bf742ba929ac2aeaaa89454a624611ca21f1
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-08 15:02:38 +0530

    Add files via upload

--- extractedfirstname.py
+++ extractedfirstname.py
@@ -1,5 +1,15 @@
-first_name= "Maria"
-last_name= "S"
+"""
+program name : extracting first name
+version: 1.1
+date: 8-10-2024
+This is a python program that create two separate strings,concatenating string,from the concatenated string:
+Access and print a sub-string that consists of the first name only.
+"""
+
+
+
+first_name= input("enter your first name")
+last_name= input("enter your last name")
 full_name= first_name+ " "+ last_name
 print(full_name)
 length= len(first_name)
