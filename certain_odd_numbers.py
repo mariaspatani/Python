38efe597cbe43a45ad06f7b235bff1b9c380e221
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-15 16:11:45 +0530

    Add files via upload

---
+++ certain_odd_numbers.py
@@ -0,0 +1,11 @@
+"""
+author= maria
+Python program to generate certain number of odd numbers.
+Version 1.1
+Date : 15/10/2024
+"""
+limit= int(input("enter the limit:"))
+odd_number =1
+while odd_number<=limit:
+    print(odd_number, "\t",end="")
+    odd_number+=2
