d667917164cc1356931ecea1f0d8a7e6cbf36769
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-15 15:26:07 +0530

    Add files via upload

---
+++ greaterornot.py
@@ -0,0 +1,14 @@
+"""
+Python program to compare whether it is greater number or not
+Version 1.1
+Date-15/10/2024
+"""
+Number1=int(input("enter a numbers"))
+Number2=int(input("enter another number"))
+if Number1>Number2:
+    print(Number1,"is greater than",Number2)
+elif Number1<Number2:
+    print(Number2,"is greater than",Number1)
+else:
+    print("The given numbers are equal")
+
