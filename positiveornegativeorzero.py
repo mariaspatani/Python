16d2ed042ce95b0d25930f481b67b875369cd937
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-15 14:13:16 +0530

    Add files via upload

---
+++ positiveornegativeorzero.py
@@ -0,0 +1,12 @@
+"""
+Python program to check whether the number is positive or negative or Zero
+version 1.1
+Date - 15/10/2024
+"""
+number= int(input("Enter a number:"))
+if number>0:
+    print("The given number:",number,"is positive")
+elif number<0:
+    print("The given number:",number,"is negative")
+else:
+    print("The given number:",number,"is Zero")
