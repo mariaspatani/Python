523f2abcb9942fd7c9c4b5b38c4dc80192fe9d7f
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-15 15:25:22 +0530

    Add files via upload

---
+++ python11.py
@@ -0,0 +1,12 @@
+"""
+Python program to check whether the person is minor or major
+Version 1.1
+Date- 15/10/2024
+
+"""
+Age= int(input("Enter your age"))
+if Age>=18:
+    print("You are a major")
+else:
+    print("You are a minor")
+
