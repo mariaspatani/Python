573bbd3773c6aa499036baecd1c88ff31af7cc30
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-15 15:28:11 +0530

    Add files via upload

---
+++ driving_license.py
@@ -0,0 +1,10 @@
+"""
+Python program to check if a person is eligible for driving license.
+Version 1.1
+Date 15/10/2024
+"""
+Age= int(input("enter the age"))
+if Age>16:
+    print("Eligible for a driving license")
+else:
+    print("Not eligible for a driving license")
