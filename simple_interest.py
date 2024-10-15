b1b6e83c72759b1bbe382011c1a0f0565ea59c71
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-15 15:29:56 +0530

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,10 @@
+"""
+Python program to find the simple interest
+Version 1.1
+Date 15/10/2024
+"""
+principle_value=int(input("enter the principle value"))
+rate=int(input("enter rate"))
+number_of_years=int(input("enter the number of years"))
+simple_interest=principle_value*number_of_years*rate
+print(simple_interest)
