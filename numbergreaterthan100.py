783e8788f155718df3396ca3045da59710476207
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-15 15:27:41 +0530

    Add files via upload

---
+++ numbergreaterthan100.py
@@ -0,0 +1,8 @@
+"""
+Python program to check if the number is greater than 100
+Version 1.1
+Date 15/10/2024
+"""
+number= int(input("enter a number"))
+if number>100:
+    print("Number is greater than 100")
