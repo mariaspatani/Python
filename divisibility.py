f4819d6e703fa6af2f5c63fe53aa58294af023fe
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-15 15:29:05 +0530

    Add files via upload

---
+++ divisibility.py
@@ -0,0 +1,10 @@
+"""
+Python program to check whether a number is divisible by 5 or not.
+Version 1.1
+Date 15/10/2024
+"""
+number=int(input("enter a number"))
+if number%5==0:
+    print("Number divisible by 5")
+else:
+    print("Number not divisible by 5")
