f667f29bda2e875f3900988611fdfc090ff90438
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-21 23:15:26 +0530

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,11 @@
+"""
+author:maria
+date :21/10/2024
+version:1.1
+Python program that checks if a person has passsed or failes an exam.
+"""
+mark= int(input("enter your marks"))
+if mark>=40:
+    print("pass")
+else:
+    print("fail")
