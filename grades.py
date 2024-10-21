eb4f801d1e2e07927f99275e695c4118101106c8
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-21 23:06:28 +0530

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,13 @@
+"""
+author:maria
+date: 21/10/2024
+version:1.1
+Python program that assigns a grade based on the marks obtained by a student.
+"""
+
+
+grade=int(input("Enter your mark"))
+if grade>=90:
+    print("Grade:A")
+else:
+    print("Below A Grade")
