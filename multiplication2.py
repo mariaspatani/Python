edf49c02b1351db220a00a8b945fc4b91b3e1d25
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-29 16:01:52 +0530

    Add files via upload

---
+++ multiplication2.py
@@ -0,0 +1,11 @@
+"""
+author : maria
+date : 29/10/2024
+version : 1.1
+This is a python program to print a multiplication of a number.
+"""
+
+number=int(input("enter a number"))
+for i in range(1,13):
+     ans=number*i
+     print(number,"*",i,"\t=\t",ans)
