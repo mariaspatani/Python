c371ca23b98daf47ecf6584491772b9797ea22f7
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-10-29 15:05:22 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,15 @@
+"""
+author: maria
+date: 29/10/2024
+version: 1.1
+This is a python program to check whether give number is prime or not.
+"""
+limit=int(input("enter a number"))
+for number in range(2,limit):
+    isPrime= True
+    for i in range(2,(number//2+1)):
+        if number%i ==0:
+            isPrime = False
+            break
+    if isPrime:
+            print(number,end=" ")
