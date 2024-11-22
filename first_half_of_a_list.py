303813aa37474719282a8b54036f4052fcec1fc3
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-11-22 22:30:02 +0530

    Add files via upload

---
+++ first_half_of_a_list.py
@@ -0,0 +1,10 @@
+"""
+Author: Maria
+Date:22/11/2024
+Version :1.1
+This is a python program to print first half of a list.
+"""
+list1=[1,2,3,4,5,6,7,8,9,1]
+print(len(list1))
+A=len(list1)//2
+print(list1[0:A])
