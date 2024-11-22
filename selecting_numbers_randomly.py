10762cbec3cc5a1a92e04ab9a7afd3c7e2ca84bf
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-11-22 22:30:28 +0530

    Add files via upload

---
+++ selecting_numbers_randomly.py
@@ -0,0 +1,9 @@
+"""
+Author: Maria
+Date:22/11/2024
+Version :1.1
+This is a python program to select second and third numbers from a random choice.
+"""
+list1=[1,4,9,5,6,7,8,2,3]
+print(list1[1:3])
+
