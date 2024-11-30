4678bce92b02248d739f13cf241c3ae05d3bfe75
mariaspatani <mariaspatani2028@cs.sjcetpalai.ac.in>
2024-11-30 10:15:52 +0530

    Add files via upload

---
+++ right_triangle.py
@@ -0,0 +1,20 @@
+"""
+Author:Maria
+Date:30/11/2024
+Version:1.1
+This is a python program to check whether given sides are part of a right angled triangle .
+"""
+
+def is_right_angle_triangle(side1,side2,side3):
+    sides =[side1,side2,side3]
+    sides.sort()
+    if sides[2]**2==sides[0]**2 + sides[1]**2:
+        return True
+    return False
+side1=int(input("enter side1"))
+side2=int(input("enter side2"))
+side3=int(input("enter side3"))
+if is_right_angle_triangle(side1,side2,side3):
+    print("The Triangle is a right angled triangle")
+else:
+    print("The Triangle is not a right angled triangle3")
